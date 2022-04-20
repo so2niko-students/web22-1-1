cdf244cb5ebbf2733dbee70a8e76901d996a4443
Nick Sotula <so2niko@gmail.com>
2022-04-20 19:07:27 +0300

    random elements

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 ##Web22-1-1
 
 End of js web course
+
+  
+| Team  | Member | Member |
+|--|--|--|
+| Українські ніндзя-єноти | Artem | Karen |
+|--|--|--|
+| Чорноброві бджоли | Alexander | Danil |
+|--|--| -- |
+| 2Є | Egor | Elizaveta |
+
+
---
+++ app/main.js
@@ -0,0 +1,10 @@
+const r0_10 = Math.floor(Math.random() * (10 - 0) + 0); //[0,10)
+const r0_20 = Math.floor(Math.random() * 20 + 0); //[0,20)
+const r0_100 = Math.floor(Math.random() * 100 + 0); //[0,100)
+const r1_10 = Math.floor(Math.random() * (10 - 1) + 1); //[1,10)
+const r5_9 = Math.floor(Math.random() * (9 - 5) + 5); //[5,9)
+const r15_30 = Math.floor(Math.random() * (30 - 15 + 1) + 15); //[15,30]
+
+
+console.log(r15_30);
+
