d9dda7b51828ca18e20840e5a6cefb5293a7e421
Nick Sotula <so2niko@gmail.com>
2022-04-20 19:54:25 +0300

    feat: app vs random generator

--- app/main.js
+++ app/main.js
@@ -1,10 +1,28 @@
-const r0_10 = Math.floor(Math.random() * (10 - 0) + 0); //[0,10)
-const r0_20 = Math.floor(Math.random() * 20 + 0); //[0,20)
-const r0_100 = Math.floor(Math.random() * 100 + 0); //[0,100)
-const r1_10 = Math.floor(Math.random() * (10 - 1) + 1); //[1,10)
-const r5_9 = Math.floor(Math.random() * (9 - 5) + 5); //[5,9)
-const r15_30 = Math.floor(Math.random() * (30 - 15 + 1) + 15); //[15,30]
+// const r0_10 = Math.floor(Math.random() * (10 - 0) + 0); //[0,10)
+// const r0_20 = Math.floor(Math.random() * 20 + 0); //[0,20)
+// const r0_100 = Math.floor(Math.random() * 100 + 0); //[0,100)
+// const r1_10 = Math.floor(Math.random() * (10 - 1) + 1); //[1,10)
+// const r5_9 = Math.floor(Math.random() * (9 - 5) + 5); //[5,9)
+// const r15_30 = Math.floor(Math.random() * (30 - 15 + 1) + 15); //[15,30]
+
+//!---------------------------------------------------------------------------
+//!---------------------------------------------------------------------------
+const random = (from, to) => Math.floor(Math.random() * (to - from + 1) + from);
+
+const MY_EL = {
+    h1      : document.querySelector('.answer'),
+    inpFrom : document.querySelector('.inp-from'),
+    inpTo   : document.querySelector('.inp-to'),
+    btnGen  : document.querySelector('.btn-generate')
+};
 
 
-console.log(r15_30);
+MY_EL.btnGen.addEventListener('click', () => {
+    const from = MY_EL.inpFrom.value * 1;
+    const to = MY_EL.inpTo.value * 1;
 
+    const rnd = random(from, to);
+
+    MY_EL.h1.innerText = rnd;
+});
+ 
