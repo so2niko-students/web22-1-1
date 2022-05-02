c11109ac7e030f36cbec3c299868eedf24b1047c
Nick Sotula <so2niko@gmail.com>
2022-05-02 18:59:04 +0300

    conditions

---
+++ main.js
@@ -0,0 +1,97 @@
+var a = 3;
+
+let b = 3;
+const c = 4;
+
+const d = {};
+
+d.name = 'lalal';
+
+const e = [1, 3, 'haha'];
+
+e.push('another data');
+
+//--------------------------
+// Conditions
+
+if(true){
+    //
+}else{
+    //
+}
+
+if(true){
+    //
+}
+
+true ? console.log('yes') : console.log('no');
+
+switch(123){
+    case 100 : {
+        //
+        break;
+    }
+    case 37 : {
+        //
+        break;
+    }
+    case 123: {
+        //
+        break;
+    }
+    default:{
+        //Действие по умолчанию.
+    }
+}
+
+const input = 1 * prompt('hello');
+switch(input){
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 2 : {
+        console.log('Лютий, Февраль, February');
+        break;
+    }
+    case 3 : {
+        console.log('Березень, Март, March');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+    case 1 : {
+        console.log('Грудень, Январь, January');
+        break;
+    }
+}
