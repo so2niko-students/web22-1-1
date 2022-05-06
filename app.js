ee6545820903da6e2e1c1973b2000848bdeb364b
Nick Sotula <so2niko@gmail.com>
2022-05-06 18:00:14 +0300

    20220506 add images of classes and roles

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-const rand = (from, to) => Math.floor((Math.random() * (to - from + 1)) + from);
-const randArr = arr => arr[rand(0, arr.length - 1)];
+//MVC
+//M - model(data), V - view(render data), C - Controller (mediator)
 
-const people = ['Nick', 'Alex', 'Egor', 'Artem', 'Eliz'];
-const races = ['Human', 'Elf', 'Dark Elf', 'Dwarf', 'Orc', 'Undead'];
-const roles = ['DD', 'Heal', 'Tank', 'RDD'];
-const classes = {
-    'DD' : ['Paladin', 'Monk', 'Druid', 'Warrior'],
-    'Heal' : ['Paladin', 'Priest', 'Druid'],
-    'Tank' : ['Paladin', 'Druid', 'Warrior'],
-    'RDD' : ['Mage', 'Priest', 'Druid', 'Warlock']
+const model = () => {
+    const rand = (from, to) => Math.floor((Math.random() * (to - from + 1)) + from);
+    const randArr = arr => arr[rand(0, arr.length - 1)];
+    
+    const people = ['Nick', 'Alex', 'Egor', 'Artem', 'Eliz'];
+    const races = ['Human', 'Elf', 'Dark Elf', 'Dwarf', 'Orc', 'Undead'];
+    const roles = ['DD', 'Heal', 'Tank', 'RDD'];
+    const classes = {
+        'DD' : ['Paladin', 'Barbarian', 'Assassin', 'Rogue', 'Warrior'],
+        'Heal' : ['Paladin', 'Priest'],
+        'Tank' : ['Paladin', 'Barbarian', 'Warrior'],
+        'RDD' : ['Mage', 'Priest', 'Necromanser', 'Hunter', 'Assassin', 'Rogue']
+    }
+    
+    const users = people.map(name => {
+        const role = randArr(roles);
+        return  {
+            name, role,
+            race    : randArr(races),
+            classUser   : randArr(classes[role])
+        }
+    });
+
+    return users;
 }
 
-const users = people.map(name => {
-    const role = randArr(roles);
-    return  {
-        name    : name,
-        race    : randArr(races),
-        role,
-        class   : randArr(classes[role])
-    }
-});
+const view = users => {4
+    const domUsers = document.querySelector('.users');
 
-console.table(users);
+    const htmlUsers = users.map(({ name, race, role, classUser }) => {
+        return `<div>
+            <img src="img/${ classUser }.jpg" class="img_class">
+            <h2>${ name }</h3>
+            <h4>${ race } ${ role } ${ classUser }</h4>
+            <img src="img/${ role.toLowerCase() }.png" class="img_role">
+        </div>`;
+    }).join('');
+
+    domUsers.innerHTML = htmlUsers;
+}
+
+const controller = () => {
+    const users = model();
+    view(users);
+}
+
+controller();
