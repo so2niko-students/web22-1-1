907c0613553c0e7eee06967602ee3478bf3d31da
Nick Sotula <so2niko@gmail.com>
2022-05-27 16:57:55 +0300

    fix problem with entering pure sitename

--- app/main.js
+++ app/main.js
@@ -5,11 +5,13 @@ const textIconURL = document.querySelector('.text-icon-url');
 const BASE_ICON_URL = 'https://icon.horse/icon/';
 
 const onLickBtnFind = () => {
-    const url = new URL(inputURL.value);
+    const rawUrl = inputURL.value;
+    const regExpUrl = /http[s]?:/;
+    const url = regExpUrl.test(rawUrl)? new URL(rawUrl).host : rawUrl;
     imgIcon.src = '';
-    textIconURL.innerText = url.host;
+    textIconURL.innerText = url;
     inputURL.value = '';
-    imgIcon.src = `${ BASE_ICON_URL }${ url.host }`;
+    imgIcon.src = `${ BASE_ICON_URL }${ url }`;
 }
 
 btnFind.addEventListener('click', onLickBtnFind);
